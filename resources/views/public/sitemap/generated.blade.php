5fb5dfc95aba040bf291ac469725d636127af751
Ayana Ozawa <ayana.ozawa@gmail.com>
2026-08-04 13:58:38 +0900

    Add newline after sitemap XML declaration

--- resources/views/public/sitemap/generated.blade.php
+++ resources/views/public/sitemap/generated.blade.php
@@ -1,5 +1,5 @@
 @php
-    echo '<?xml version="1.0" encoding="UTF-8"?>';
+    echo '<?xml version="1.0" encoding="UTF-8"?>' . PHP_EOL;
 @endphp
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
 @foreach ($urls as $url)
